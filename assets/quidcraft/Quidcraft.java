d890ae10bf4ff3469c2c19a3b893b3887a2b8369
GotoLink <gotolinkminecraft@gmail.com>
2014-04-04 17:15:42 +0200

    M.U.D support

--- assets/quidcraft/Quidcraft.java
+++ assets/quidcraft/Quidcraft.java
@@ -1,12 +1,14 @@
 package assets.quidcraft;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "quidcraft", name = "Quidcraft", version = "0.2")
+@Mod(modid = "quidcraft", name = "Quidcraft", useMetadata = true)
 public class Quidcraft {
     @Mod.Instance(value="quidcraft")
     public static Quidcraft INSTANCE;
@@ -21,5 +23,15 @@ public class Quidcraft {
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		proxy.preInit();
+        if(event.getSourceFile().getName().endsWith(".jar") && event.getSide().isClient()){
+            try {
+                Class.forName("mods.mud.ModUpdateDetector").getDeclaredMethod("registerMod", ModContainer.class, String.class, String.class).invoke(null,
+                        FMLCommonHandler.instance().findContainerFor(this),
+                        "https://raw.github.com/GotoLink/Quidcraft/master/update.xml",
+                        "https://raw.github.com/GotoLink/Quidcraft/master/changelog.md"
+                );
+            } catch (Throwable e) {
+            }
+        }
 	}
 }
